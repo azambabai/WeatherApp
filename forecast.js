b7e337d42ce42b50b4ae7d1888b93d4e0b2f740c
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-05-06 22:07:54 +0430

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -1,4 +1,10 @@
 const key = 'LnyKUw4on8IyWVIodsApwac49ez3bgmN';
 
+//get weather information
+const getWeather = async (id)=>{
 
+const base ='http://dataservice.accuweather.com/currentconditions/v1/';
+const query = `${id}?apikey=${key}`;
+const response = await fetch( base + query);
+const data = await response.json();
 
