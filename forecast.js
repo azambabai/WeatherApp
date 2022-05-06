1ac31d520565d5ef9c300b13cc67270530437348
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-05-06 22:08:35 +0430

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -11,3 +11,15 @@ const data = await response.json();
 // console.log(data);
 return data[0];
 }
+
+//get city information
+const getCity = async (city) =>{
+
+    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+   return data[0];
+};
