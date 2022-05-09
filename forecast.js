bb9d9049b140dc29bbcb2c78feec3cd1d907b9c1
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-05-09 15:50:38 +0430

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -16,7 +16,6 @@ const getCity = async (city) =>{
 
     const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
-
     const response = await fetch(base + query);
     const data = await response.json();
    return data[0];
